717dbf71361fd60103ad101317da3444aa6fb15f
Lou1sThomas <louisthomas26@outlook.com>
2024-11-30 22:45:31 +0000

    Modified PartTimeEmployee.

--- src/PartTimeEmployee.java
+++ src/PartTimeEmployee.java
@@ -3,14 +3,14 @@ public class PartTimeEmployee extends Employee {
     private double hourlyPay;
     private double hoursWorked;
 
-    // Constructor
-    public PartTimeEmployee(String name, int employeeId, String email, String position, int level, boolean promotion, double hoursWorked, double hourlyPay) {
-        super(name, employeeId, email, position, level, false, false);
+    // Constructor for PartTimeEmployee
+    public PartTimeEmployee(String name, int employeeId, String email, String position, int level, boolean promotion, boolean fullTime, double hoursWorked, double hourlyPay) {
+        super(name, employeeId, email, position, level, promotion, fullTime);  // Call superclass constructor
         this.hoursWorked = hoursWorked;
         this.hourlyPay = hourlyPay;
     }
 
-
+    // Factory method to create PartTimeEmployee from a CSV string
     public static PartTimeEmployee fromCSV(String csvLine) {
         String[] fields = csvLine.split(",");
         return new PartTimeEmployee(
@@ -18,27 +18,26 @@ public class PartTimeEmployee extends Employee {
                 Integer.parseInt(fields[0]), // employeeId
                 fields[2], // email
                 fields[3], // position
-                Boolean.parseBoolean(fields[4]), // promotion
-                Double.parseDouble(fields[5]), // hoursWorked
-                Double.parseDouble(fields[6])  // hourlyPay
+                Integer.parseInt(fields[4]), // level
+                Boolean.parseBoolean(fields[5]), // promotion
+                Boolean.parseBoolean(fields[6]), // fullTime (needs to be correctly parsed from CSV)
+                Double.parseDouble(fields[7]), // hoursWorked
+                Double.parseDouble(fields[8])  // hourlyPay
         );
     }
 
-
+    // Method to calculate salary based on hours worked and hourly pay
     @Override
     public double calculateSalary() {
         double grossSalary = hoursWorked * hourlyPay;
         Deductions deductions = new Deductions();
         return deductions.calcDeductions(grossSalary);
-
     }
 
-    public double
-
-
+    // Method to convert PartTimeEmployee to a CSV string
     @Override
     public String toCSV() {
         return getEmployeeId() + "," + getName() + "," + getEmail() + "," + getPosition() + ","
-                + isPromotion() + "," + hoursWorked + "," + hourlyPay;
+                + getLevel() + "," + isFullTime() + "," + isPromotion() + "," + hoursWorked + "," + hourlyPay;
     }
 }
