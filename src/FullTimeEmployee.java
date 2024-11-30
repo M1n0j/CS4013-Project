0ac25a3246fd4957b02555e07a6815cb141269e6
ocounihan <orancounihan@gmail.com>
2024-11-30 19:58:14 +0000

    FullTimeEmployee with the calculateSalary method

--- src/FullTimeEmployee.java
+++ src/FullTimeEmployee.java
@@ -1,9 +1,10 @@
+import java.util.List;
 
 public class FullTimeEmployee extends Employee {
 
-    public FullTimeEmployee(String name, int employeeId, String email,  String position, boolean promotion, int level, boolean fullTime) {
+    public FullTimeEmployee(String name, int employeeId, String email, String position, boolean promotion, int level, boolean fullTime) {
 
-        super(name, employeeId, email, position, promotion, fullTime);
+        super(name, employeeId, email, position, level, promotion, fullTime);
     }
 
     // Instance method to create a FullTimeEmployee from a CSV line
@@ -22,7 +23,26 @@ public class FullTimeEmployee extends Employee {
 
     @Override
     public double calculateSalary() {
-        return 0; // Implement salary calculation logic here
+        List<SalaryScale> salaryScaleList = CSVReader.readSalaryScales("src/Resources/Salaries.csv");
+
+        double grossSalary = 0.0;
+        double netSalary = 0.0;
+        for(SalaryScale s : salaryScaleList) {
+            if (s.getPosition().equals(this.getPosition()) && s.getLevel() == this.getLevel()) {
+                grossSalary = s.getSalary();
+                break;
+            }
+        }
+
+        if (grossSalary == 0.0) {
+            System.err.println("No Salary Found");
+            return 0.0;
+        }
+        Deductions deductions = new Deductions();
+        double totalDeductions = deductions.calcDeductions(grossSalary);
+        netSalary = grossSalary - totalDeductions;
+
+        return netSalary;
     }
 
     @Override
