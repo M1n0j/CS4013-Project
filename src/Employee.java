62b8cf808bdd90bf68f80665941917daf258519d
Lou1sThomas <louisthomas26@outlook.com>
2024-11-30 18:52:04 +0000

    Working Employee class

--- src/Employee.java
+++ src/Employee.java
@@ -4,11 +4,10 @@ public abstract class Employee {
     private String name;
     private int employeeId;
     private String employeePassword;
-    private LocalDate dateOfBirth;
     private String email;
-    private String department;
     private String position;
     private boolean promotion;
+    private boolean fullTime;
 
 
     // Constructor with all fields
@@ -16,21 +15,10 @@ public abstract class Employee {
         this.name = name;
         this.employeeId = employeeId;
         this.email = email;
-        this.department = department;
         this.position = position;
         this.promotion = promotion;
         this.employeePassword = employeePassword;
-    }
-
-    // Default constructor
-    public Employee() {
-        this.name = "";
-        this.employeeId = 0;
-        this.dateOfBirth = LocalDate.now(); // Default to the current date
-        this.email = "";
-        this.department = "";
-        this.position = "";
-        this.promotion = false;
+        this.fullTime = fullTime;
     }
 
     // Getters
@@ -42,25 +30,19 @@ public abstract class Employee {
         return employeeId;
     }
 
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-
-
     public String getEmail() {
         return email;
-    }
-
-
-
-    public String getDepartment() {
-        return department;
     }
 
     public String getPosition() {
         return position;
     }
+
+    public String getEmployeePassword() {return employeePassword;}
+
+    public boolean isPromotion() {return promotion;}
+
+    public boolean isFullTime() {return fullTime;}
 
     // Setters
     public void setName(String name) {
@@ -76,12 +58,6 @@ public abstract class Employee {
         this.email = email;
     }
 
-
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
     public void setPosition(String position) {
         this.position = position;
     }
@@ -90,25 +66,11 @@ public abstract class Employee {
         this.promotion = promotion;
     }
 
-    public boolean getPromotion() {
-        return promotion;
-    }
-
+    public void setFullTime(boolean fullTime) {this.fullTime = fullTime;}
 
 
     // Abstract method to calculate salary (to be implemented by subclasses)
     public abstract double calculateSalary();
-
-    // Method to print employee details
-    public void printEmployee() {
-        System.out.println("Name: " + name);
-        System.out.println("Employee ID: " + employeeId);
-        System.out.println("Date of Birth: " + dateOfBirth);
-        System.out.println("Email: " + email);
-        System.out.println("Department: " + department);
-        System.out.println("Position: " + position);
-        System.out.println("Promotion: " + promotion);
-    }
 
     public abstract String toCSV();
 }
