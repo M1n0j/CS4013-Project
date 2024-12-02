c4adab8e53214d844104b257f809b88248c66f41
Lou1sThomas <louisthomas26@outlook.com>
2024-12-02 09:53:01 +0000

    Added toCSV method to payslip

--- src/Payslip.java
+++ src/Payslip.java
@@ -1,3 +1,5 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class Payslip {
     private static double grossSalary;
@@ -17,15 +19,15 @@ public class Payslip {
 
 
 
-    public static void printPayslip() {
+    public void printPayslip() {
         System.out.println("\n----- Payslip -----");
-        System.out.println("Gross Salary: €" + String.format("%.2f", grossSalary));
-        System.out.println("PRSI: €" + String.format("%.2f", deductions.getPrsi()));
-        System.out.println("USC: €" + String.format("%.2f", deductions.getUsc()));
-        System.out.println("Income Tax: €" + String.format("%.2f", deductions.getIncomeTax()));
-        System.out.println("Union Fee: €" + String.format("%.2f", deductions.getUnionFee()));
-        System.out.println("Total Deductions: €" + String.format("%.2f", deductions.getTotalDeductions()));
-        System.out.println("Net Salary: €" + String.format("%.2f", netSalary));
+        System.out.println("Gross Salary: €" + String.format("%.2f", this.grossSalary));
+        System.out.println("PRSI: €" + String.format("%.2f", this.deductions.getPrsi()));
+        System.out.println("USC: €" + String.format("%.2f", this.deductions.getUsc()));
+        System.out.println("Income Tax: €" + String.format("%.2f", this.deductions.getIncomeTax()));
+        System.out.println("Union Fee: €" + String.format("%.2f", this.deductions.getUnionFee()));
+        System.out.println("Total Deductions: €" + String.format("%.2f", this.deductions.getTotalDeductions()));
+        System.out.println("Net Salary: €" + String.format("%.2f", this.netSalary));
         System.out.println("-------------------");
     }
 
@@ -40,4 +42,22 @@ public class Payslip {
            String.format("%.2f", netSalary);
 
     } 
+
+    public void writePayslipToCSV(String filename) {
+        try (PrintWriter writer = new PrintWriter(filename)) {
+        writer.println("Gross Salary,PRSI,USC,Income Tax,Union Fee,Total Deductions,Net Salary");
+        writer.println(toCSV());
+        writer.println("\n----- Payslip -----");
+        writer.println("Gross Salary: €" + String.format("%.2f", this.grossSalary));
+        writer.println("PRSI: €" + String.format("%.2f", this.deductions.getPrsi()));
+        writer.println("USC: €" + String.format("%.2f", this.deductions.getUsc()));
+        writer.println("Income Tax: €" + String.format("%.2f", this.deductions.getIncomeTax()));
+        writer.println("Union Fee: €" + String.format("%.2f", this.deductions.getUnionFee()));
+        writer.println("Total Deductions: €" + String.format("%.2f", this.deductions.getTotalDeductions()));
+        writer.println("Net Salary: €" + String.format("%.2f", this.netSalary));
+        writer.println("-------------------");
+    } catch (FileNotFoundException e) {
+        System.out.println("Error writing to file: " + e.getMessage());
+        }
+    }
 }
