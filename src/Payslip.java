f79d5a721c9ceb2ff86b0ed00b90e264b73bee18
ocounihan <orancounihan@gmail.com>
2024-12-02 07:26:27 +0000

    Fix to how Payslip calls Deductions methods

--- src/Payslip.java
+++ src/Payslip.java
@@ -1,41 +1,32 @@
 import java.time.LocalDate;
 
 public class Payslip {
-    private int employeeId;
-    private double grossPay;
-    private double netPay;
-    private int payslipId;
-    private String name;
-    private LocalDate payPeriod;
+    private double grossSalary;
+    private double netSalary;
     private Deductions deductions;
 
-    public Payslip(int employeeId, double grossPay, double netPay, int payslipId, String name, LocalDate payPeriod) {
-        this.employeeId = employeeId;
-        this.grossPay = grossPay;
-        this.netPay = netPay;
-        this.payslipId = payslipId;
-        this.name = name;
-        this.payPeriod = payPeriod;
+    public Payslip(double grossSalary) {
+        this.grossSalary = grossSalary;
         this.deductions = new Deductions();
+        calculateDeductions();
+        this.netSalary = grossSalary - deductions.getTotalDeductions();
     }
 
-    private void calculateDeductions() {
-        deductions.setPrsi(grossPay * 0.04);
-        deductions.setUsc(grossPay * 0.02);
-        deductions.setIncomeTax(grossPay * 0.20);
-
-        netPay = grossPay - deductions.calcDeductions(grossPay);
+    public void calculateDeductions() {
+        deductions.calcDeductions(grossSalary);
     }
 
-    public void newPayslip() {
-        System.out.println("--------PAYSLIP--------");
-        System.out.println("Employee ID: " + employeeId);
-        System.out.println("Payment Date: " + payPeriod);
-        System.out.println("Gross Pay: €" + String.format("%.2f", grossPay));
-        System.out.println("------Deductions:------");
+
+
+    public void printPayslip() {
+        System.out.println("----- Payslip -----");
+        System.out.println("Gross Salary: €" + String.format("%.2f", grossSalary));
         System.out.println("PRSI: €" + String.format("%.2f", deductions.getPrsi()));
         System.out.println("USC: €" + String.format("%.2f", deductions.getUsc()));
         System.out.println("Income Tax: €" + String.format("%.2f", deductions.getIncomeTax()));
-        System.out.println("Net Pay: €" + String.format("%.2f", netPay));
+        System.out.println("Union Fee: €" + String.format("%.2f", deductions.getUnionFee()));
+        System.out.println("Total Deductions: €" + String.format("%.2f", deductions.getTotalDeductions()));
+        System.out.println("Net Salary: €" + String.format("%.2f", netSalary));
+        System.out.println("-------------------");
     }
 }
